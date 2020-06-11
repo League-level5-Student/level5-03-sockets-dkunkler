846635b533c8bbdb19e72a935f9526d2c04e5c20
David Kunkler <dkunkler@github.com>
2020-06-10 19:59:06 -0700

    Button Clicker,intro to sockets

--- src/_01_Intro_To_Sockets/server/ServerGreeter.java
+++ src/_01_Intro_To_Sockets/server/ServerGreeter.java
@@ -5,25 +5,26 @@ import java.io.*;
 
 public class ServerGreeter extends Thread {
 	//1. Create an object of the ServerSocket class
-
+	ServerSocket serverSocket;
 	public ServerGreeter() throws IOException {
 		//2. Initialize the ServerSocket object. In the parameters,
 		//   you must define the port at which the server will listen for connections.
-		
+		serverSocket = new ServerSocket(8080);
 		//*OPTIONAL* you can set a time limit for the server to wait by using the 
 		//  ServerSocket's setSoTimeout(int timeInMilliSeconds) method
+		serverSocket.setSoTimeout(60000);
 	}
 
 	public void run() {
 		//3. Create a boolean variable and initialize it to true.
-		
+		boolean b = true;
 		//4. Make a while loop that continues looping as long as the boolean created in the previous step is true.
-			
+		while(b){	
 			//5. Make a try-catch block that checks for two types Exceptions: SocketTimeoutException and IOException.
 			//   Put steps 8 - 15 in the try block.
-		
+			try {
 				//8. Let the user know that the server is waiting for a client to connect.
-		
+				
 				//9. Create an object of the Socket class and initialize it to serverSocket.accept();
 				//   Change serverSocket to match the ServerSocket member variable you created in step 1.
 				//   The program will wait her until either a client connects or the timeout expires.
@@ -39,12 +40,22 @@ public class ServerGreeter extends Thread {
 				//14. Use the DataOutputStream object to send a message to the client using the writeUTF(String message) method.
 				
 				//15. Close the client server
-		
+		System.out.println("Server waiting for client to connect");
+				Socket connection = serverSocket.accept();
+				serverSocket = 
+				
+				
+				
+			} catch (IOException e) {
+				// TODO: handle exception
+				
+				b = false;
+			}
 			//6. If the program catches a SockeTimeoutException, let the user know about it and set loop's boolean variable to false.
 
 			//7. If the program catches a IOException, let the user know about it and set the loop's boolean variable to false.
 
-		
+		}
 	}
 
 	public static void main(String[] args) {
